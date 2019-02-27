fc08bfeeffdef03f85bc298105ab2ba0adc81d24
Rick Button <me@rickbutton.me>
2019-02-27 00:19:19 -0500

    updated csproj

--- src/workspacer.Native/Native/WindowsManager.cs
+++ src/workspacer.Native/Native/WindowsManager.cs
@@ -4,7 +4,6 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net.Configuration;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
